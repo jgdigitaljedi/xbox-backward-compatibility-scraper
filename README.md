8256527e4e0ea884651c87f5fa00740b01e4d204
Joey Gauthier <digitaljedi@outlook.com>
2019-06-12 07:25:45 -0500

    got first list data, need writeFile functionality and second list now

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Xbox Backward Compatibility Scraper
 
-The intent of this script is to scrape 2 lists from the official Xbox website to collect the following data:
-- Xbox games that work on the Xbox 360
-- Xbox and Xbox 360 games that work on the Xbox One
+The intent of this script is to scrape 2 lists from the corresponding Wikipedia pages to collect the following data:
+- Xbox games that work on the Xbox 360 and their issues
+- Xbox and Xbox 360 games that work on the Xbox One and their issues
 
 It's not the most useful data in the world, but I am a retro game collector and wanted access to this data for a little project I'm working on. Rather than copy, paste, and cleanup, this script will allow me to generate the data which will be handy is it is still being updated for the Xbox One.
 
--- index.js
+++ index.js
@@ -1,6 +1,50 @@
 (function () {
   const cheerio = require('cheerio');
-  const axios = require('axios');
+  const request = require('request');
+  const chalk = require('chalk');
+  const fs = require('fs');
 
+  function makeRequest(url) {
+    return new Promise((resolve, reject) => {
+      request(
+        url,
+        (err, response, html) => {
+          if (err) {
+            reject({ error: true, code: err });
+          } else {
+            resolve(html);
+          }
+        }
+      )
+    });
+  }
+
+  // get the Xbox games that run on the Xbox 360
+  makeRequest(`https://en.wikipedia.org/wiki/List_of_Xbox_games_compatible_with_Xbox_360`)
+    .then(html => {
+      const $ = cheerio.load(html);
+      const rows = $('.wikitable.sortable').filter(i => i === 0).find('tbody').find('tr');
+      // parse the table rows
+      let result = [];
+      $(rows).each((index, element) => {
+        if (index > 0) {
+          const rowData = $(element).text().split('\n');
+          const game = rowData[1];
+          const issues = rowData[5];
+          // create json object turning issues into an array based on the fact that wiki seems to list each issue as a sentence in a string
+          const entry = {
+            game,
+            problems: issues === '' ? null : issues.split('.').map(i => i.trim()).filter(i => i !== '')
+          };
+          console.log('entry', entry);
+          result.push(entry);
+        }
+      });
+      // write to a json file
+
+    })
+    .catch(error => {
+      console.log(chalk.red.bold('ERROR GETTING 360 BC LIST: ', error));
+    });
 
 })();
